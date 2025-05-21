fa9df5c5d5119e4c60ec5e1319e158b30b630c85
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-05-21 19:41:37 +0300

    project setup

---
+++ app/src/main/java/com/muhammed/android_compose_navigation_task/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.muhammed.android_compose_navigation_task
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.muhammed.android_compose_navigation_task.ui.theme.AndroidComposeNavigationTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidComposeNavigationTaskTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidComposeNavigationTaskTheme {
+        Greeting("Android")
+    }
+}
